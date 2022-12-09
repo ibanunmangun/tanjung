efdc88747b09316b0213305cae1645abcf6ae473
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-12-10 03:36:16 +0800

    Add files via upload

---
+++ data/poipapuma_3.js
@@ -0,0 +1,19 @@
+var json_poipapuma_3 = {
+"type": "FeatureCollection",
+"name": "poipapuma_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Vihara Dewi Sri Wulan", "cmt": "Point of Interest", "desc": "Vihara Papuma, HHC3+49M, Area Kebun, Lojejer, Kec. Wuluhan, Kabupaten Jember, Jawa Timur 68172, Indonesia", "x": "113.55337", "y": "-8.4296537" }, "geometry": { "type": "Point", "coordinates": [ 113.55337, -8.4296537 ] } },
+{ "type": "Feature", "properties": { "name": "Pantai Tanjung Papuma", "cmt": "Point of Interest", "desc": "Papuma Beach, East Java, Indonesia", "x": "113.5536464", "y": "-8.4300871" }, "geometry": { "type": "Point", "coordinates": [ 113.5536464, -8.4300871 ] } },
+{ "type": "Feature", "properties": { "name": "Caffe Evergreen Papuma", "cmt": "Point of Interest", "desc": "Caffe Evergreen Papuma, HH93+F3G, Area Wisata, Jalan Watuloh, Jl. Tj. Papuma, Area Kebun, Lojejer, Kec. Wuluhan, Kabupaten Jember, Jawa Timur 68172, Indonesia", "x": "113.5526615", "y": "-8.431323" }, "geometry": { "type": "Point", "coordinates": [ 113.5526615, -8.431323 ] } },
+{ "type": "Feature", "properties": { "name": "Siti Hinggil Papuma", "cmt": "Point of Interest", "desc": "Siti Hinggil Papuma, HH73+H68, Area Kebun, Lojejer, Kec. Wuluhan, Kabupaten Jember, Jawa Timur 68172, Indonesia", "x": "113.5530748", "y": "-8.4360754" }, "geometry": { "type": "Point", "coordinates": [ 113.5530748, -8.4360754 ] } },
+{ "type": "Feature", "properties": { "name": "Vihara Papuma", "cmt": "Public Facilities", "desc": null, "x": "113.55331463635189", "y": "-8.429567111842545" }, "geometry": { "type": "Point", "coordinates": [ 113.553314636351885, -8.429567111842545 ] } },
+{ "type": "Feature", "properties": { "name": "Mushola Papuma", "cmt": "Public Facilities", "desc": null, "x": "113.5519992", "y": "-8.431362686" }, "geometry": { "type": "Point", "coordinates": [ 113.5519992, -8.431362686 ] } },
+{ "type": "Feature", "properties": { "name": "Pemadam Kebakaran Pos Ambulu", "cmt": "Public Facilities", "desc": null, "x": "113.6076606", "y": "-8.339276721" }, "geometry": { "type": "Point", "coordinates": [ 113.6076606, -8.339276721 ] } },
+{ "type": "Feature", "properties": { "name": "Parkir Kawasan", "cmt": "Public Facilities", "desc": null, "x": "113.5526743", "y": "-8.429662999" }, "geometry": { "type": "Point", "coordinates": [ 113.5526743, -8.429662999 ] } },
+{ "type": "Feature", "properties": { "name": "Klinik Rawat Inap Mestra (Bu Mita)", "cmt": "Public Facilities", "desc": null, "x": "113.5512165", "y": "-8.380696091" }, "geometry": { "type": "Point", "coordinates": [ 113.5512165, -8.380696091 ] } },
+{ "type": "Feature", "properties": { "name": "Dr. Luthfi", "cmt": "Public Facilities", "desc": null, "x": "113.5937578", "y": "-8.392791091" }, "geometry": { "type": "Point", "coordinates": [ 113.5937578, -8.392791091 ] } },
+{ "type": "Feature", "properties": { "name": "PErtashop SP.681.19", "cmt": "Public Facilities", "desc": null, "x": "113.5863812", "y": "-8.388755107" }, "geometry": { "type": "Point", "coordinates": [ 113.5863812, -8.388755107 ] } },
+{ "type": "Feature", "properties": { "name": "Polsek Ambulu", "cmt": "Public Facilities", "desc": null, "x": "113.6064051", "y": "-8.344343607" }, "geometry": { "type": "Point", "coordinates": [ 113.6064051, -8.344343607 ] } }
+]
+}
